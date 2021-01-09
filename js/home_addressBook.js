516ebba477adc3abba2ca5b52ac614f2ce2fc333
chetan <zambare.c123@gmail.com>
2021-01-09 19:56:32 +0530

    [Chetan] Added: Add employee from Local storage

--- js/home_addressBook.js
+++ js/home_addressBook.js
@@ -1,6 +1,6 @@
 let addressbookList ;
 window.addEventListener('DOMContentLoaded',(event)=>{
-  //addressbookList = getAddressDataFromStorages();
+  addressbookList = getAddressDataFromStorages();
   createInnerHtml();
 });
 
@@ -9,40 +9,28 @@ const getAddressDataFromStorages = () =>{
   }
 
 const createInnerHtml= () => {
-  //if (addressbookList.length == 0) return;
+  if (addressbookList.length == 0) return;
   const headerHtml = "<th>Full Name </th> <th>Address</th> <th>City</th> <th>State</th>"+
                         "<th>Zip</th> <th>Phone Number</th> <th>Actions</th>";
   let innerHtml = `${headerHtml}`;
 
-  //for(const contact of addressbookList){
+  for(const contact of addressbookList){
     innerHtml = `${innerHtml}
       <tr>
-        <td>Chetan Zambare</td>
-        <td>Shree Nagar Bhusawal</td>
-        <td>Bhusawal</div>
-        <td>Maharstra</td>
-        <td>425201</td>
-        <td>7741833846</td>
+        <td>${contact._name}</td>
+        <td>${contact._address}</td>
+        <td>${contact._city}</div>
+        <td>${contact._state}</td>
+        <td>${contact._zip}</td>
+        <td>${contact._phone}</td>
         <td>
           <img id="1" onclick="remove(this)" alt="delete" src="../assests/icons/delete-black-18dp.svg">
           <img id="1"  onclick="update(this)" alt="edit" src="../assests/icons/create-black-18dp.svg">
         </td>
       </tr>
-      <tr>
-      <td>Chetan Zambare</td>
-      <td>Shree Nagar Bhusawal</td>
-      <td>Bhusawal</div>
-      <td>Maharstra</td>
-      <td>425201</td>
-      <td>7741833846</td>
-      <td>
-        <img id="1" onclick="remove(this)" alt="delete" src="../assests/icons/delete-black-18dp.svg">
-        <img id="1"  onclick="update(this)" alt="edit" src="../assests/icons/create-black-18dp.svg">
-      </td>
-    </tr>
     `;
 
     document.querySelector('#table-display').innerHTML = innerHtml;
-//}
+}
 }
 
